51abb372b3cffaaf73a7cf82cba59d9b26205985
thkais <37836969+thkais@users.noreply.github.com>
2019-05-28 14:36:42 +0200

    Update Knotech.ts

--- Knotech.ts
+++ Knotech.ts
@@ -1,3 +1,4 @@
+// V1.0.5
 let KInitialized = 0
 let KLedState = 0
 let KFunkAktiv = 0
@@ -95,8 +96,8 @@ enum KCheck {
     greaterThan
 }
 
-//% color="#FF0000" icon="\uf013" block="Calli:test"
-namespace Callitest {
+//% color="#FF0000" icon="\uf013" block="Calli:bot"
+namespace Callibot {
 
     function KInit() {
         if (KInitialized != 1) {
@@ -111,7 +112,6 @@ namespace Callitest {
     function writeMotor(nr: KMotor, direction: KDir, speed: number) {
         let buffer = pins.createBuffer(3)
         KInit()
-        //basic.pause(10)
         buffer[1] = direction;
         buffer[2] = speed;
         switch (nr) {
@@ -157,7 +157,6 @@ namespace Callitest {
     export function setLed(led: KMotor, state: KState) {
         let buffer = pins.createBuffer(2)
         KInit()
-        //basic.pause(10)
         buffer[0] = 0;      // SubAddress of LEDs
         //buffer[1]  Bit 0/1 = state of LEDs
         switch (led) {
@@ -198,7 +197,6 @@ namespace Callitest {
         let len = 0;
 
         KInit()
-        //basic.pause(10)
         if (intensity < 0) {
             intensity = 0;
         }
@@ -262,16 +260,15 @@ namespace Callitest {
             buffer[3] = buffer[1];
             buffer[4] = buffer[1];
         }
-        pins.i2cWriteBuffer(0x21, buffer)
-        basic.pause(10)
+        pins.i2cWriteBuffer(0x21, buffer);
+        basic.pause(10);
     }
 
     //="Liniensensor $sensor"
     //% blockId K_readLineSensor block="Liniensensor |%sensor| |%status"
     export function readLineSensor(sensor: KSensor, status: KSensorStatus): boolean {
         let result = false
-
-        //basic.pause(10)
+        
         let buffer = pins.i2cReadBuffer(0x21, 1);
         KInit();
         if (sensor == KSensor.links) {
@@ -305,7 +302,6 @@ namespace Callitest {
     export function entfernung(modus: KEinheit): number {
         let buffer = pins.i2cReadBuffer(0x21, 3)
         KInit()
-        //basic.pause(10)
         if (modus == KEinheit.mm) {
             return 256 * buffer[1] + buffer[2]
         }
@@ -381,16 +377,16 @@ namespace Callitest {
         })
         radio.setGroup(gruppe)
         while (1 == 1) {
-            if (KFunkAktiv == 0) {
+            if (KFunkAktiv == 0){
                 if (MotorLinks < 0) {
-                    motor(KMotor.rechts, KDir.vorwärts, Math.abs(MotorLinks))
+                    Callibot.motor(KMotor.rechts, KDir.vorwärts, Math.abs(MotorLinks))
                 } else {
-                    motor(KMotor.rechts, KDir.rückwärts, MotorLinks)
+                    Callibot.motor(KMotor.rechts, KDir.rückwärts, MotorLinks)
                 }
                 if (MotorRechts < 0) {
-                    motor(KMotor.links, KDir.vorwärts, Math.abs(MotorRechts))
+                    Callibot.motor(KMotor.links, KDir.vorwärts, Math.abs(MotorRechts))
                 } else {
-                    motor(KMotor.links, KDir.rückwärts, MotorRechts)
+                    Callibot.motor(KMotor.links, KDir.rückwärts, MotorRechts)
                 }
                 basic.pause(1)
                 if (Zeit > 0) {
@@ -440,8 +436,8 @@ namespace Callitest {
     }
 
     //% blockId=K_Fernsteuerung_Status color="#E3008C" block="Schalte Empfänger |%status"
-    export function empfaengerStatus(status: KFunk) {
-        if (status == KFunk.an) {
+    export function empfaengerStatus(status : KFunk) {
+        if (status == KFunk.an){
             KFunkAktiv = 0
         }
         else {
